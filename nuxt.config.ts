9f445c7300f011a5dd35f79b0edd4c1fe00e8630
YanisRichard <yanis.richard17@gmail.com>
2024-12-02 14:10:50 +0100

    ajout du chemin pour les components

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/eslint'],
   css: ['@/assets/scss/main.scss'] ,
+  components: [
+    {path: '~/components/', pathPrefix: false}
+  ],
   vite:{
     css: {
       preprocessorOptions: {
