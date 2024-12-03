71a2887a846106d9fe34da9d5fb6ca9039c1533e
YanisRichard <yanis.richard17@gmail.com>
2024-12-03 15:00:16 +0100

    correction pour error.vue

---
+++ error.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app';
+
+defineProps({ 
+  error: {
+    type: Object as () => NuxtError,
+    default: null
+  }
+})
+
+
+const handleError = () => {
+  clearError({ redirect: '/' }); 
+}
+</script>
+
+<template>
+  <div v-if="error" class="p-error">
+    Ma page d'erreur {{ error.statusCode }} personnalisée
+    <br>
+    message : {{ error.statusMessage }}
+    <br>
+    <Button @click="handleError">Retour à la page d'accueil</Button>
+  </div>
+</template>
+
+<style lang="scss">
+</style>
