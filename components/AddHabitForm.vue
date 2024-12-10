4a05f57117ca9fe28590f37f553a213668057b02
YanisRichard <yanis.richard17@gmail.com>
2024-12-10 16:39:54 +0100

    ajout d'un composable pour le script des pages

--- components/AddHabitForm.vue
+++ components/AddHabitForm.vue
@@ -1,30 +1,18 @@
 <script lang="ts" setup="">
+const title = ref("");
+const description = ref("");
 
 const emit = defineEmits(['habit:created'])
 
-const title = ref("");
-const description = ref("");
-const error = ref<string | null>(null);
 
 async function onSubmit(event: Event) {
   event.preventDefault();
 
   try {
-    const response = await fetch("http://localhost:4000/habits", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-        Authorization: `Bearer ${useCookie("api_tracking_jwt").value}`,
-      },
-      body: JSON.stringify({
-        title: title.value,
-        description: description.value,
-      }),
-    });
-
-    if (!response.ok) throw new Error("Une erreur est survenue");
-
-    await response.json;
+    await useTrackingApi('/habits', {
+        method: 'POST',
+        body: { title: title.value, description: description.value}
+    })
 
     emit('habit:created')
 
